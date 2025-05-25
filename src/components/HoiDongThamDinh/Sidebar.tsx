07caffcaeac387a3f9c57e99dee47a2ac5d77c87
Bùi Yến Nhi <buidoanhaiyen@gmail.com>
2025-05-25 15:37:39 +0700

    Update

--- src/components/HoiDongThamDinh/Sidebar.tsx
+++ src/components/HoiDongThamDinh/Sidebar.tsx
@@ -6,30 +6,15 @@ const navigationItems = [
   {
     icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/eab83ce3769a682628746e736e2c44dbf0bdad9e?placeholderIfAbsent=true",
     label: "Trang chủ",
-<<<<<<< HEAD
     active: true,
-=======
->>>>>>> a1b7d63de158f78babe3a12a91009ecbc5b150d0
   },
   {
     icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/f3e88bfa9ae998efe3390d97326af3323f959e8e?placeholderIfAbsent=true",
     label: "Thông tin cá nhân",
   },
   {
-    icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/548976d608c31c0e145581e31116c4038add5580?placeholderIfAbsent=true",
-    label: "Dự án",
-  },
-  {
-    icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/0c3c53abe8367c35966fb2cd9e6e663a994463d9?placeholderIfAbsent=true",
-    label: "Đội",
-  },
-  {
     icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/483d2bb0cfca4845368f546fb5cd08b5e805477f?placeholderIfAbsent=true",
     label: "Đánh giá",
-<<<<<<< HEAD
-=======
-    active: true,
->>>>>>> a1b7d63de158f78babe3a12a91009ecbc5b150d0
   },
   {
     icon: "https://cdn.builder.io/api/v1/image/assets/823bf4beb2774bc99c68daa06d856dec/e84f75a28ad473788606c62583a5fed72ab176a5?placeholderIfAbsent=true",
